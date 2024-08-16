ed4adf4a0de237735062dbab4abb6dcbd496b568
Jussi Aalto <jussi.aalto@taitounited.fi>
2024-08-16 14:41:44 +0300

    add freedoom wads

--- dsda-doom/flake.nix
+++ dsda-doom/flake.nix
@@ -11,9 +11,30 @@
       repo = "dsda-doom";
       flake = false;
     };
+
+    freedoom-src = {
+      type = "github";
+      owner = "freedoom";
+      repo = "freedoom";
+      flake = false;
+    };
+
+    deutex-src = {
+      type = "github";
+      owner = "Doom-Utils";
+      repo = "deutex";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, dsda-src }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    dsda-src,
+    freedoom-src,
+    deutex-src
+  }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
 
@@ -32,6 +53,52 @@
           ];
 
           sourceRoot = "source/prboom2";
+        };
+
+        packages.freedoom-wads = pkgs.stdenv.mkDerivation {
+          name = "freedoom-wads";
+          src = freedoom-src;
+
+          nativeBuildInputs = with pkgs; [
+            packages.deutex
+            python3
+            asciidoc
+            asciidoctor
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.pillow
+            ]))
+          ];
+
+          preBuild = ''
+            patchShebangs \
+            scripts/simplecpp \
+            graphics/text/textgen \
+            graphics/text/tint.py \
+            graphics/text/smtextgen \
+            graphics/text/rotate \
+            graphics/text/create_caption \
+            lumps/playpal/playpal \
+            lumps/colormap/colormap \
+            lumps/genmidi/mkgenmidi \
+            lumps/dmxgus/gen-ultramid \
+            lumps/textures/build-textures \
+            bootstrap/bootstrap \
+            dist/pillow-resize \
+            dist/pillow-compose
+          '';
+
+          DESTDIR = "$(out)";
+        };
+
+        packages.deutex = pkgs.stdenv.mkDerivation {
+          name = "deutex";
+          src = deutex-src;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            autoreconfHook
+            libpng.dev
+          ];
         };
 
         packages.default = packages.dsda-doom;
