823eb1290558fef778ec79dd7ad0b6f5c08d7dd4
Jussi Aalto <j.v.aalto@gmail.com>
2024-09-25 21:45:26 +0300

    nbd

---
+++ nbd/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "flake for building nbd client and server";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nbd-src = {
+      type = "github";
+      owner = "NetworkBlockDevice";
+      repo = "nbd";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nbd-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+
+      in rec {
+        flakedPkgs = pkgs;
+
+	packages.nbd = pkgs.stdenv.mkDerivation {
+          name = "nbd";
+          src = nbd-src;
+
+	  nativeBuildInputs = with pkgs; [
+            pkg-config
+	    flex
+            bison
+	    #git
+	    autoconf-archive
+	    autoreconfHook
+          ];
+
+	  buildInputs = with pkgs; [
+	    glib
+	    gnutls
+	    libnl
+	    linuxHeaders
+	  ];
+
+	  configureFlags = [
+	    "--sysconfdir=/etc"
+	  ];
+        };
+
+	packages.default = packages.nbd;
+      }
+    );
+}
