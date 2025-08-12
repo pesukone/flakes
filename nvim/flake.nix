f4cea2c6d5c037e5dc1db2fb08c4bcd8be474d59
Jussi Aalto <jussi.aalto@taitounited.fi>
2025-08-12 09:07:10 +0300

    add mini.pairs

--- nvim/flake.nix
+++ nvim/flake.nix
@@ -11,6 +11,15 @@
       url = "https://luarocks.org/manifests/neorocks/mini.indentscope-0.16.0-1.rockspec";
       flake = false;
     };
+
+    mini-pairs-src = {
+      url = "github:echasnovski/mini.pairs?ref=refs/tags/v0.16.0";
+      flake = false;
+    };
+    mini-pairs-rockspec = {
+      url = "https://luarocks.org/manifests/neorocks/mini.pairs-0.16.0-1.rockspec";
+      flake = false;
+    };
   };
 
   outputs =
@@ -20,12 +29,15 @@
       flake-utils,
       mini-indentscope-src,
       mini-indentscope-rockspec,
+      mini-pairs-src,
+      mini-pairs-rockspec,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        rockspecFilename = "mini.indentscope-0.16.0-1.rockspec";
+        indentscopeRockspec = "mini.indentscope-0.16.0-1.rockspec";
+        pairsRockspec = "mini.pairs-0.16.0-1.rockspec";
       in
       {
         flakedPkgs = pkgs;
@@ -34,11 +46,26 @@
           luaAttr = pkgs.lua.pkgs.buildLuarocksPackage {
             pname = "mini.indentscope";
             version = "0.16.0-1";
-            rockspecFilename = rockspecFilename;
+            rockspecFilename = indentscopeRockspec;
             src = mini-indentscope-src;
 
             preInstall = ''
-              cp ${mini-indentscope-rockspec} ${rockspecFilename}
+              cp ${mini-indentscope-rockspec} ${indentscopeRockspec}
+            '';
+
+            disabled = pkgs.lua.pkgs.luaOlder "5.1";
+          };
+        };
+
+        packages.mini-pairs = pkgs.neovimUtils.buildNeovimPlugin {
+          luaAttr = pkgs.lua.pkgs.buildLuarocksPackage {
+            pname = "mini.pairs";
+            version = "0.16.0-1";
+            rockspecFilename = pairsRockspec;
+            src = mini-pairs-src;
+
+            preInstall = ''
+              cp ${mini-pairs-rockspec} ${pairsRockspec}
             '';
 
             disabled = pkgs.lua.pkgs.luaOlder "5.1";
