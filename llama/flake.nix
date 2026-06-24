d445f15835babbaeb2ec83899fa00f78708e17c8
pesukone <j.v.aalto@gmail.com>
2026-06-24 11:52:18 +0300

    wip: llama

---
+++ llama/flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "llama.cpp flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    llama-src = {
+      url = "github:ggml-org/llama.cpp";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      llama-src,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+      in
+      rec {
+        packages.llama = pkgs.stdenv.mkDerivation {
+          name = "llama.cpp";
+          src = llama-src;
+          nativeBuildInputs = with pkgs; [ cmake ];
+
+          buildInputs = with pkgs; [
+            rocmPackages.clr
+            rocmPackages.rocblas
+            rocmPackages.hipblas
+          ];
+
+          cmakeFlags = [
+            (pkgs.lib.strings.cmakeBool "GGML_NATIVE" false)
+            (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_EXAMPLES" false)
+            (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_SERVER" true)
+            (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_TESTS" false)
+            (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_OPENSSL" true)
+            (pkgs.lib.strings.cmakeBool "GGML_BLAS" true)
+            (pkgs.lib.strings.cmakeBool "GGML_HIP" true)
+            (pkgs.lib.strings.cmakeBool "GGML_RPC" true)
+            (pkgs.lib.strings.cmakeBool "GGML_VULKAN" true)
+            (pkgs.lib.strings.cmakeFeature "CMAKE_HIP_COMPILER" "${pkgs.rocmPackages.llvm.clang}/bin/clang")
+            (pkgs.lib.strings.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx1201;gfx1030")
+          ];
+
+          env = {
+            ROCM_PATH = "${pkgs.rocmPackages.clr}";
+            HIP_DEVICE_LIB_PATH = "${pkgs.rocmPackages.rocm-device-libs}/amdgcn/bitcode";
+          };
+        };
+
+        packages.default = packages.llama;
+      }
+    );
+}
