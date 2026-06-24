5766fc636bc7f65b4c3336f83505b8de82002183
Jussi Aalto <j.v.aalto@gmail.com>
2026-06-24 17:11:12 +0300

    llama.cpp

--- llama/flake.nix
+++ llama/flake.nix
@@ -28,15 +28,26 @@
         packages.llama = pkgs.stdenv.mkDerivation {
           name = "llama.cpp";
           src = llama-src;
-          nativeBuildInputs = with pkgs; [ cmake ];
+          enableParallelBuilding = true;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+          ];
 
           buildInputs = with pkgs; [
             rocmPackages.clr
             rocmPackages.rocblas
             rocmPackages.hipblas
+            #rocmPackages.rocwmma
+            openblas
+            vulkan-headers
+            vulkan-loader
+            shaderc
           ];
 
           cmakeFlags = [
+            (pkgs.lib.strings.cmakeFeature "CMAKE_BUILD_TYPE" "Release")
             (pkgs.lib.strings.cmakeBool "GGML_NATIVE" false)
             (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_EXAMPLES" false)
             (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_SERVER" true)
@@ -44,8 +55,10 @@
             (pkgs.lib.strings.cmakeBool "LLAMA_BUILD_OPENSSL" true)
             (pkgs.lib.strings.cmakeBool "GGML_BLAS" true)
             (pkgs.lib.strings.cmakeBool "GGML_HIP" true)
+            #(pkgs.lib.strings.cmakeBool "GGML_HIP_ROCWMMA_FATTN" true)
             (pkgs.lib.strings.cmakeBool "GGML_RPC" true)
             (pkgs.lib.strings.cmakeBool "GGML_VULKAN" true)
+            (pkgs.lib.strings.cmakeBool "GGML_BACKEND_DL" true)
             (pkgs.lib.strings.cmakeFeature "CMAKE_HIP_COMPILER" "${pkgs.rocmPackages.llvm.clang}/bin/clang")
             (pkgs.lib.strings.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx1201;gfx1030")
           ];
