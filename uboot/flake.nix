043179cca9d0d258e5cf6122050de925364cfff3
pesukone <j.v.aalto@gmail.com>
2026-07-26 17:57:50 +0300

    uboot for rk1

--- uboot/flake.nix
+++ uboot/flake.nix
@@ -1,12 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    uboot-src = {
-      url = "github:u-boot/u-boot?ref=refs/tags/v2025.07";
+    u-boot-src = {
+      url = "github:u-boot/u-boot?ref=refs/tags/v2026.04";
       flake = false;
     };
     rkbin-src = {
       url = "github:rockchip-linux/rkbin";
+      flake = false;
+    };
+    optee-src = {
+      url = "github:OP-TEE/optee_os";
+      flake = false;
+    };
+    atf-src = {
+      url = "github:ARM-software/arm-trusted-firmware";
       flake = false;
     };
   };
@@ -15,17 +23,28 @@
     {
       self,
       nixpkgs,
-      uboot-src,
+      u-boot-src,
       rkbin-src,
+      optee-src,
+      atf-src,
     }:
     let
+      /*
+        aarch64-pkgs = (
+          import nixpkgs {
+            localSystem = "aarch64-linux";
+            crossSystem = "arm-linux-gnueabihf";
+            config.allowUnsupportedSystem = true;
+          }
+        );
+      */
       aarch64-pkgs = nixpkgs.legacyPackages."aarch64-linux";
       armv7-pkgs = nixpkgs.legacyPackages."armv7l-linux";
     in
-    {
-      packages."aarch64-linux".uboot-quartz64 = aarch64-pkgs.stdenv.mkDerivation {
+    rec {
+      packages."aarch64-linux".u-boot-quartz64 = aarch64-pkgs.stdenv.mkDerivation {
         name = "u-boot-quartz64";
-        src = uboot-src;
+        src = u-boot-src;
 
         nativeBuildInputs = with aarch64-pkgs; [
           bison
@@ -48,7 +67,7 @@
           patchShebangs tools/binman
         '';
 
-        configurePhase = ''
+        preConfigure = ''
           export ROCKCHIP_TPL="$(ls ${rkbin-src}/bin/rk35/rk3566_ddr_1056MHz_v*.bin | sort | tail -n1)"
           export BL31="$(ls ${rkbin-src}/bin/rk35/rk3568_bl31_v*.elf | sort | tail -n1)"
 
@@ -64,9 +83,120 @@
         '';
       };
 
-      packages."armv7l-linux".uboot-rpi2 = armv7-pkgs.stdenv.mkDerivation {
+      devShells."aarch64-linux".default = aarch64-pkgs.mkShell {
+        inputsFrom = [ packages."aarch64-linux".u-boot-quartz64 ];
+      };
+
+      packages."armv7l-linux".u-boot-rpi2 = armv7-pkgs.stdenv.mkDerivation {
         name = "u-boot-rpi2";
-        src = uboot-src;
+        src = u-boot-src;
+
+        nativeBuildInputs = with armv7-pkgs; [
+          bison
+          flex
+          swig
+          (python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              setuptools
+              pyelftools
+            ]
+          ))
+        ];
+
+        buildInputs = with armv7-pkgs; [
+          openssl.dev
+          gnutls.dev
+        ];
+
+        preConfigure = ''
+          make rpi_2_defconfig
+        '';
+
+        buildPhase = ''
+          make -j$(nproc)
+        '';
+
+        installPhase = ''
+          cp u-boot.bin $out
+        '';
+      };
+
+      packages."aarch64-linux".optee = aarch64-pkgs.stdenv.mkDerivation {
+        name = "OP-TEE";
+        src = optee-src;
+
+        nativeBuildInputs = with aarch64-pkgs; [
+          (python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              cryptography
+              pyelftools
+            ]
+          ))
+        ];
+
+        postPatch = ''
+          patchShebangs scripts
+        '';
+
+        makeFlags = [
+          "CFG_ARM64_core=y"
+          "PLATFORM=rockchip"
+          "PLATFORM_FLAVOR=rk3588"
+        ];
+      };
+
+      packages."aarch64-linux".atf = aarch64-pkgs.stdenv.mkDerivation {
+        name = "arm-trusted-firmware";
+        src = atf-src;
+
+        buildPhase = ''
+          make PLAT=rk3588 bl31 -j
+        '';
+      };
+
+      packages."aarch64-linux".u-boot-rk1 = aarch64-pkgs.stdenv.mkDerivation {
+        name = "u-boot-rk1";
+        src = u-boot-src;
+
+        nativeBuildInputs = with aarch64-pkgs; [
+          bison
+          flex
+          swig
+          (python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              setuptools
+              pyelftools
+            ]
+          ))
+        ];
+
+        buildInputs = with aarch64-pkgs; [
+          openssl.dev
+          gnutls.dev
+        ];
+
+        postPatch = ''
+          patchShebangs tools/binman
+          patchShebangs scripts
+        '';
+
+        preConfigure = ''
+          make ARCH=arm64 turing-rk1-rk3588_defconfig
+        '';
+
+        buildPhase = ''
+          export ROCKCHIP_TPL="$(ls ${rkbin-src}/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v*.bin | sort | tail -n1)"
+          export BL31="$(ls ${rkbin-src}/bin/rk35/rk3588_bl31_v*.elf | sort | tail -n1)"
+
+          ARCH=arm64 make -j$(nproc)
+        '';
+
+        installPhase = ''
+          dd if=/dev/zero of=bootloader.img bs=512 count=32767
+          dd if=./idbloader.img of=bootloader.img bs=512 seek=64 conv=notrunc
+          dd if=./u-boot.itb of=bootloader.img bs=512 seek=16384
+          cp bootloader.img $out
+        '';
       };
     };
 }
